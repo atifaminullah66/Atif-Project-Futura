cf8f80de0c2777aee7e4d4249f423c0fc35af8cb
Atif <atif.aminullah49@gmail.com>
2023-10-03 15:09:17 +0200

    add build steps in github action pipeline

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/Atif-Project-Futura",
+  images: {
+     unoptimzied: true,
+  },
 }
 
 module.exports = nextConfig
